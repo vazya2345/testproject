7d417ce60439cc71868e8867149ddb71d3291007
Тухтасинов Авазбек Ойбек угли <10927@bank.local>
2021-01-19 09:14:49 +0500

    commit2

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 				<div class="row">
 				  <div class="col-md-2 offset-md-10">
 				  	
-				  	<button id='inpsubmit' class="col-md-12 btn btn-danger">Записать</button>
+				  	<button id='inpsubmit' class="col-md-12 btn btn-danger">Записать?</button>
 				  </div>
 				</div>
 				
